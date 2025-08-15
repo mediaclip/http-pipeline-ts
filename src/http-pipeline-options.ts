77ffb572668a87c5c08eabb507e9c02be04c5066
Bertrand Provost <bertrand.provost@mediaclip.ca>
2025-08-14 21:06:20 -0400

    Fix on nodejs, window is not available

--- src/http-pipeline-options.ts
+++ src/http-pipeline-options.ts
@@ -1,7 +1,7 @@
 import { HttpPipelinePolicy } from "./policies";
 
 export class HttpPipelineOptions {
-    public fetch: typeof window.fetch;
+    public fetch: typeof globalThis.fetch;
     public retryCount = 3;
     public retryableStatusCode = [-1, 0, 408, 502, 503, 504];
     public timeoutMs = 15_000;
@@ -9,7 +9,7 @@ export class HttpPipelineOptions {
     public perCallPolicies: HttpPipelinePolicy[] = [];
     public retryPolicy?: HttpPipelinePolicy;
 
-    constructor(fetch?: typeof window.fetch) {
-        this.fetch = fetch ?? window.fetch;
+    constructor(customFetch?: typeof globalThis.fetch) {
+        this.fetch = customFetch ?? globalThis.fetch ?? fetch;
     }
 }
